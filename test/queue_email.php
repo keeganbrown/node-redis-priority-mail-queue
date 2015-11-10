367aac744498301fc4d62dbb0be9079d9117b57c
Keegan Brown <brown.keegan@gmail.com>
2015-11-09 18:25:54 -0600

    back to 1000

--- test/queue_email.php
+++ test/queue_email.php
@@ -36,9 +36,9 @@ function queue_email($email, $priority = '9')
  * Grab and push test email data into redis
  * @param $limit_rounds number of time function will execute recurstively
  */
-function init ( $limit_rounds = 10 ) {
+function init ( $limit_rounds = 1 ) {
 	$limit_rounds--;
-	$email_data_array = json_decode( exec('node test/make-test-data.js 100') );
+	$email_data_array = json_decode( exec('node test/make-test-data.js 1000') );
 	
 	foreach ($email_data_array as $key => $email_data_item) {
 		queue_email( $email_data_item, rand(0,10) );
